f8404a2e51e217a290f91bec49923b628323ef67
Alvaro Lopez <lt0895032020@unab.edu.sv>
2021-08-27 14:17:39 -0600

    vistas

---
+++ Lab1-CalculadoraNotas/Controllers/NotasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_CalculadoraNotas.Controllers
+{
+    public class NotasController : Controller
+    {
+        // GET: Notas
+        public ActionResult Index()
+        {
+
+            
+            return View();
+        }
+        public ActionResult Resultado()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
